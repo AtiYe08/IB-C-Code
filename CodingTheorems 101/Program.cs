6b1df195bd1689f6e91afd1aac367c85b3a061e1
AtiYe08 <153394072+AtiYe08@users.noreply.github.com>
2023-12-17 14:14:24 +0100

    Add files via upload

---
+++ CodingTheorems 101/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CodingTheorems_101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int[] x = { 1, 2, 3, 4, 5, 6 };
+            int sum = 0;
+            int a = 0;
+            while (a<6)
+            {
+                sum = sum + x[a];
+                a++;
+            }
+            Console.WriteLine(sum);*/
+
+            /* int[] array = { 1, 2, 3, 4, 5, 6 };
+             int sum = 0;
+             for(int i=0;i<6;i++)
+             {
+                 sum = sum + array[i];
+             }
+             Console.WriteLine(sum);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6, 7};
+            int count = 0;
+            for (int i=0;i<array.Length;i++)
+            {
+                count = count + 1;
+            }
+            Console.WriteLine(count);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            int count = 0;
+            int i =0;
+            while (i < array.Length)
+            { 
+                count = count +1;
+                i++;
+            }
+            Console.WriteLine(count);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            int product = 1;
+            for (int i = 0; i<array.Length;i++)
+            {
+                product = product * array[i];
+            }
+            Console.WriteLine(product);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6, 7 };
+            int product = 1;
+            int i = 0;
+            while (i < array.Length)
+            {
+                product = product * array[i];
+                i++;
+            }
+            Console.WriteLine(product);*/
+
+            /*int[] array = { 7,6565,334,2,99653,8384 };
+            int min = array[0];
+            for (int i = 0;i<array.Length;i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            Console.WriteLine(min);*/
+
+            /*int[] array = { 7, 6565, 334, 2, 99653, 8384 };
+            int max = array[0];
+            for (int i = 0;i<array.Length;i++)
+            {
+                if (array[i] > max)
+                { 
+                max= array[i];
+                }
+            }
+            Console.WriteLine(max);*/
+
+            /*int[] array = { 1, 2, 3, 4, 5, 6 };
+            int toselect = 1;
+            int i = 0;
+            while (array[i] != toselect)
+            {
+                i++;
+            }
+            Console.WriteLine(i);*/
+
+
+
+
+        }
+    }
+}
+
+
+
+
+
+   
